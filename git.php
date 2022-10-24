a5753d7d71431a190c048509824a9cc990bc4213
OsamaAbdAlMalik-hup <106681811+OsamaAbdAlMalik-hup@users.noreply.github.com>
2022-10-24 15:05:23 -0700

    final mondy

--- git.php
+++ git.php
@@ -8,11 +8,13 @@
              * revert change any time and merge branch and solve conflicts
 
          * operations:
-             * git clone url: get project
+             * git clone url: get project from remote repository
+             * git pull remoteName: get changes from remote repository
              * git add file: to add file to staging area  | add . add *
              * git rm --cached file: to remove file to staging area
              * git commit -m "message": to move file to local repository
              * git push remoteName branchName: to move changes branch to remote repository
+             * git push -u remoteName branchName: to move changes also but you solve conflict firstly
 
          * helpers:
              * git status: to show status of all files
@@ -20,7 +22,12 @@
              * git remote: to show remotes name of project
              * git remote -v: to show remotes name with url of project
              * git remote -v: to show remotes name with url of project
-             * git remote -v: to show remotes name with url of projects
 
+
+         * common:
+             * cd <url>: change directory
+             * mkdir <url>: make directory
+             * touch <file>: create file
+             * touch: create file
 
          */
