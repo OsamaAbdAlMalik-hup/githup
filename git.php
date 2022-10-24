6fb3c1b528d7a7d5938ecfc75c91c70cb0defbaf
OsamaAbdAlMalik-hup <106681811+OsamaAbdAlMalik-hup@users.noreply.github.com>
2022-10-24 09:37:25 -0700

    Begin github

---
+++ git.php
@@ -0,0 +1,22 @@
+<?php
+
+        /**
+         * git: version control system
+         * github: site appear cloud for remote repositories
+         * advantages git:
+             * allow contribute the project and organize team
+             * revert change any time and merge branch and solve conflicts
+
+         * operations:
+             * clone: get project :: git clone url
+             *
+
+         * helpers:
+             * git status: to show status of all files
+             * git add fileName: to add file to staging area  | add . add *
+             * git rm --cached fileName: to remove file to staging area  | add . add *
+             * git commit -m "message": to move file to local repository
+             * git push: to move changes to remote repository
+
+
+         */
