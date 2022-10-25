45e8f61f513287ec8af8f870a95deebff632a0ef
OsamaAbdAlMalik-hup <106681811+OsamaAbdAlMalik-hup@users.noreply.github.com>
2022-10-25 10:00:18 -0700

    pull

--- git.php
+++ git.php
@@ -10,18 +10,30 @@
          * operations:
              * git clone url: get project from remote repository
              * git pull remoteName: get changes from remote repository
-             * git add file: to add file to staging area  | add . add *
-             * git rm --cached file: to remove file to staging area
+             * git add <file>: to add|update file to|on staging area  | add . add *
+             * git rm --cached <file>: to remove file to staging area
              * git commit -m "message": to move file to local repository
-             * git push remoteName branchName: to move changes branch to remote repository
-             * git push -u remoteName branchName: to move changes also but you solve conflict firstly
+             * git push <remoteName> <branchName>: to move changes branch to remote repository
+             * git push -u <remoteName> <branchName>: to move changes also but you solve conflict firstly
 
          * helpers:
              * git status: to show status of all files
+
              * git branch: to show branches of project
+             * git branch <branchName>: to create branch in project
+             * git checkout -b <branchName>: to create branch and switch to it
+             * git checkout <branchName>: to switch to branch in project
+             * git branch -m <branchName>: to rename branch
+             * git branch -D <branchName>: to delete branch
+             * git branch -d <branchName>: to delete branch  but not delete if exist changes not merge
+             * git merge <branchName>: to merge branchName with current branch
+
              * git remote: to show remotes name of project
              * git remote -v: to show remotes name with url of project
              * git remote -v: to show remotes name with url of project
+
+             * git config --global alias.<name> value : to create alias for value and must be in "" if content space
+
 
 
          * common:
